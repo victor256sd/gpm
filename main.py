1b9d98c047b1becd026e12e3830fe6b8f8b34183
victor256sd <victor256@gmail.com>
2025-08-02 10:54:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def disable_button():
     st.session_state.disabled = True        
 
 def map_prep(df):
+    print("In map_prep()")
     openai_api_key = st.secrets["OPENAI_API_KEY"]
     llm = OpenAI(api_token=openai_api_key)
     pandas_ai = PandasAI(llm, conversational=False)
@@ -25,12 +26,12 @@ def map_prep(df):
     INSTRUCTION_LAT_ENCRYPTED = b'gAAAAABojD7KychbGVgX7yDF8PJGZ5MM_0fFAMyc5bQ2PkGY-TSvsQeDc5n7eKERV7hLhlRipXNHZ_CTMGVLPFiZjeduRVrnLwsp8Nwy4Z9eKzmJ0bJVXgSp53mck7fd90BxG0ZBfuzKMdT9UAIhLu2noe8gibETxKzdYwTL2El__Up22Ju6IZtBzSgs1hernZXBUgFCv0kaPagWYRIPfXhtr9a3PE6j4jwpsA56__gyNUNHGGDPI0PFVFCEgoK5qEqqq2D49EQWnG1mnYNeDLocws_Lt4XSa8dsVeVX-6BPu3AA4jrVLl2TsszUnuJGh0wrvnHuQpFjD3nZlqzvbNabN_R9hhwpVj-clfhgbeIKzm2AKr1-gsTL7pDTGpd0m4TavFNNnKdUaSjS8flcImOZGKG-kzd9EM2_8PfuabMgntCnZJbxiX8='
     INSTRUCTION_LON_ENCRYPTED = b'gAAAAABojD9rvOaMPeu4xctybHFTC1lqB_kanALn0QPI0BysUmOH4qP6PzQbx71JZwoieJ5gKXMrJYNaNH9fBYgo0ZxMArpKMLUy59x8s7UK4RzGdyI88Jm25ONiWWOtGcEeRspBVgZ4yBnE_XGA_NHE2WRz6tTB2awW54l1Wvi4cVt0839jd26K8taOqeROKnrOs0nt6hNuCWY33ujXsM_9gtqY-xGSUYCveuExgjsIJO0eBYdqz2SwkrboUudz6vmS_52ToZap16JZ4vdtypToCSGM90VQzJS30cDmajWt09E5m4dL1oC53Dah6Ta7uPyRagLtI1TMkW-I157LVXf4UY_MQu1dYhqqHrFyedx34aBJsygn0OnCyqX6YMkN00TyM6gTvYv4OKkmn3FKGoQ3XI-sd0aBXfqNgOW6K8g62CDE26xHAxc='
 
+    print("Past encrypted instrucs")
+    
     key = st.secrets['INSTRUCTION_KEY'].encode()
     f = Fernet(key)
     INSTRUCTION_LAT = f.decrypt(INSTRUCTION_LAT_ENCRYPTED).decode()
     INSTRUCTION_LON = f.decrypt(INSTRUCTION_LON_ENCRYPTED).decode()
-
-    print(df)
     
     lat = pandas_ai(df, INSTRUCTION_LAT)
     lon = pandas_ai(df, INSTRUCTION_LON)
