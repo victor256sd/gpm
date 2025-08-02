0896043684b5814a82e1c928ce1fd63dedf7a2eb
victor256sd <victor256@gmail.com>
2025-08-02 11:29:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ def map_prep(df):
 
     key = st.secrets['INSTRUCTION_KEY'].encode()
     f = Fernet(key)
-    INSTRUCTION_LAT = f.decrypt(INSTRUCTION_LAT_ENCRYPTED.decode())
-    INSTRUCTION_LON = f.decrypt(INSTRUCTION_LON_ENCRYPTED).decode())
+    INSTRUCTION_LAT = f.decrypt(INSTRUCTION_LAT_ENCRYPTED.decode()
+    INSTRUCTION_LON = f.decrypt(INSTRUCTION_LON_ENCRYPTED).decode()
     
     lat = pandas_ai(df, INSTRUCTION_LAT)
     lon = pandas_ai(df, INSTRUCTION_LON)
