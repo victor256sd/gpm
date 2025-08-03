8399fe0f32702e49de047a2c2ae9744c0f16d37a
victor256sd <victor256@gmail.com>
2025-08-02 21:04:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def disable_button():
 def map_prep(df):
     openai_api_key = st.secrets["OPENAI_API_KEY"]
     llm = OpenAI(api_token=openai_api_key)
-    pandas_ai = PandasAI(llm, conversational=False)
+    # pandas_ai = PandasAI(llm, conversational=False)
 
     # lat = []
     # lon = []
@@ -42,12 +42,20 @@ def map_prep(df):
     # Drop rows where conversion failed (so we only keep real coordinates)
     # data = data.dropna(subset=['lat', 'lon'])    
     
-    INSTRUCTION_ENCRYPTED = b'gAAAAABojtcIyaYuM7nR6r91Gm40GyCfBf8V4CQOXHO9gIipGVbOfMlw_ISbxEURHVJZWxyGsi7_xZNp26iJ94kL2uqHi7J9YPiMzZmgzTzJN8bUHPgwQkUfSobXVZRlU8w-M8kgxy3gfjAysFsE-uXMv5QBfqN62qErsUjOrlxIppwvlJf1kQ3jeTT7t8LSsjjc6f5qqwRz0Mh3MQL20kOLNcas-8_Lcf3lGfzD_NdKd4y3LHqi3OEJk4ZdpqmdV_ft5a3dt1VKxiy55WD21_b1xd4NKEtz59RcOrKWOXF6bFZfyWf4leU-N5sP7Un8VyUGzzqtbLjjxSNPl4Njj-oWKwzP2HYI6dh7tMDfa6-WBLp55dJiNHTfBYgPwWsfjkECP2KsFbN15r8GgfqRgjh_DfJa5t6teHvfDVWjKez09_If4qu2W21GipuMR6_Ai93PaDUugCAELBztlleT1eIatms2UIQrKC9SUzQMMWaoXRHZnN17JV4lqOtYNyhZ9P2fqznar8-wRbTXLnzpMfKAMu_-wwQO_t7J3wgywLjczk2gjefg3F1Dlcg6VU50AfgFiI0npQUjXu0UBjfIxG3Lnl6rm770JsaaxE5rdiapVOJhuio6WWk8bPPa6aQ2OXrlM8XZST6bEiY0VxQuwOxdue9_WC3lh2vvZpVKVO5cYTwthC7rxeKrdEwDg02E2s7OL8fZiCzOvzA3uvhIJOhX1KWS3tuFxLue_gKqg6mE9NXjpezonv1xgwlHGazmK2zT1U3nmcnlPhECfC3iei2qIlpwlV0mO43NSVWANcPytfuBVdMarA3BKSSjfEuXy2u6nCHNBHNxfeV02VmFFrlaRwdCsE84fCkDwxSNNEGRBLcw7PpjQZGKHQC6IKZn1sxP7MdrhVpUcF6C-Im1XItNrdFFhWJTdmEb-T9H5bbfEWAv6tAxkMQcRPX_qDjmeRJow7rgE-j7bf85wyDJTjZlcqdcjVL8nCvhfMT8U4KD_-2-4eAUEjvcPHL84UonMIW64MXMKfJee0ttIeCWDO1FbQuwnKmw-6UZSKYFa-8jqbZPHEu631aYyLHp8wI568Q5sVv1NDKoDpyx6Q45n51TeUIY3Z-05DmZh6TuJ9FgxmeDRAJGMuTsr7kZq3KAaEy2R_FkSfvl1br0Gr3tED7ySTub1_AoFcwqKTlxHooZ1rxMkXciHFTTvaL47UWlvYwpYngQAB0QEaSnLXtYBpunLvgUue_XXbEeGWk-OzPDv9CZjc4b2PjBhxVhQ0cnYBn5rDeKCj3g_gs-vzPIGqoJkt_Xpc8vPX4DLHNMxJMIE6FxgY-R-Lqjm0Fb8RQVSb3QbC2UcoudLJmE1Ed5s-Zf1yxZn2qKkQO0xNEbxy8Pf6pKvKHwag49xDFkZKrFZG_abL7nRWprF1GXNWjYUKykee4lL8v2NKUTcuByhFyAAPBTs3RmQgMJrIORooxFh73Cx09agzXNw6CHQuH4g-14XlO-XbhzdWMC8yQZSXQ6K5YbmHLJ5cN5pEgnoEYSLD3yvmhbSj97d0E5b94LcOMGB6EKRDxSBQP3dZ3ZfdEuNQHFCFwsdoWqZc2NYJyWzgurK9zON4GqZCMm5AV04aP1O1eQlbYTgknzvV1vMmLoX083InFVrmfH10qpkKQG19GW5btpTSpFXnrgMNVhAfZ8ebiJJf40BbdHIpjylF66exDXNSsxsQV2tvcoZvIdL8vpnALBFRFqd4A_llZLNWv2TSLySeOexNIj8Z55BLGbd0USxHtCY8-5C4EULXDF5LqB3J-Rur3RjgzwO1LStRdXtJej7Trxe2nIXeHjbNBsX7zvuvNRhLtuvXcFulxi7hk1XXE_L1LdrCeyBiP4kgR_3m4iGTA3_6oectCq3CaYfkvPpIJtw0dkXEaCZGmiFFnUNl9J2-7eM4TvS2w_ALR2MmMlxpGck0YnitkvkGTy7dr5Y2Sr4jB-rVXiK8d04NtcER2jWej2hPtybHn1VXoCiXHSgZmtZAkyvDU2QqxPdkgPRP7G4X8eqDEstaF2JEObPqSBLzARPpBcUa8vtxsr1yTXKzz3IH15sXqso-I5NyLghkdN-Ag08D8k10Lo1bYZDgP6f0QChaf_XvAqdsEOln-0dkqpShFqk8NH-_nBl-zFUTIWMzLEtO-6qpBjNroV8_NqA3GkFrR5kOOmGNXQBPlxGa3XzfZhLvmZm57Nos72R0AeGdLKBxt7z4vBeeIgrhI63Im6f6VjIzWotwKdWV3jBqyGlfTgnEK1yKyLL0OxGCtv_9tne1fzWz38OcDIsoCaC87kIN_yvRTJ8AU1j77lkVSopgGGx7V2lD5MvGbaTgPLtKY='
+    INSTRUCTION_ENCRYPTED = b'gAAAAABojt3HBuPyGlENM66GfOraZ9Np_Wq9nittsRk0nurBClZDD88i6kK2YhBC18lIqVXDbZx3eDi7-QaXt-mVbNiSo68r5uVTMFc8Tj8PIGN3aZCvGWosg4rCTmaS2vUJKxz25WaCUQ1I10Cy4nGfAhQBcBEBX9sEGWlYNWEhQ_A-WPqS02aE5BvVXWQsNnG3QcNx_nk1UqVhlu5k5-2sCUX6AfGYNDzvBAoZwz_Kj1WoCEDOROdDsI07fTC3BzHoe6kMwk0_wweetbE3Cv_sTpl8P3q2qSdP-2X8mRwej0p4rSAeJJrrA1kn7QxnP7vVJWpeuBjsCBgc-WSovauxANe8f695CnXgGsbu5ZJUhiF7KDpwQg_I-D4oKuTmqjBnx-gcKo_dxx-4MLRg40MbnYgaCCa-wJy53rhzhTWbvLKfU3pLyLJUEFgaZk5H6Hl0eE3c1EISrN7Di9_nnRyFX7GaPhcQE0qtwpOiG95_heC8fFdFS1ooNa2JNciaQG4orUXrSu0VaTukQqJ8NorVqX1QYlrPSW95i7lQ9kHogeI4kcYsfM4McPL0UfTSzcPqcMATnc_YanncbgaHPPSGRDqbZcxS2MMDVmjROdRnGvhpJrtGcgyPHa15m0kSZElPJHd_ZRSmnib7mt8sBq62gXKYCyPnMqzRmboy_0Ek21SBfXRm5cMaYGpQAzmKEdoWgSNFTLtb51SPmQqW9j1ifkoANz2Wfc4hH6_HKbLLr1TmF7N5FIAKT0mMXVAnNln-Beb24GdOZ3kUTvmPjAL3OhLkMFHDu2SfcxcsPTZHIBVrNWi0JcvHZJsG0SnOo1fXKkVMrJl_urIhGnB3nJbSjE42c3PjzL2tGwQ8VnMALNi_USZIpaubzdgAocKG8d8sw92B8PPZ6Tpr5tGSAAtBtXwqZwPuss067gbaopAZYeyyhvn8epg2B7m_J3S6FQJhh5l-J4lRRU62GcSmgncSneKfd5021dBCk72mhHLBhkR9xsqQMwkqqQYio6cMntxqxCFaA1Z-_B6O_ovSYDtxaKpKJ3DcDTv57MJxSdjzld6RQHdfkIAVVChrLSoZUHJDH4qRe9ZOSfpPya_i7QowDsjMsxo6x4aU23MA15k7v_-1xoPvhIk9bNhUTs6uIMviwq3vlRjGgqNB-1A8bFe-aLGjPWcNRp5WChfTc_bVOoQsjmfRGhyCtD4Zb-9ita9hlFqJFHapNsbd0JFcBbwGuuOYfm8MmR-9OV2-sU-oBG2R3MZ9_ZFOYOuO1S5EwKZftH6YrSl3z1jCYqlRTAUplqR1evVbmBfHpBGCeptE9bZq--2t0n7zaKERAOz-VD8qiMlMwXSj3FmfNvvX7pxl1ww_cdSk3BV-ugpDI_TydeUkTAFqFyQw-mwfUNtbnKOFZzlrBpTggg7Ux4YR3M_G9gmSOhaT_uRsqaQM2R4p9eJ-vp67y7jYhqBV8qwcRj9ZyaOBPtEhRP7LzvlYjZsAjyK8n1BWvEQijVLeTBCIxvd-Jj0ZnngJqR34HQDK0nGoWDje9C3Rgs1WAB6ckISWutbHnO6nP5abNKNTxTokONGMgBccogQhZANhle6ALUcEEvupQw-Fmlu8eNuqE2tPNnpfB0Cat1Oil1izfWG1U3F88ornTK1AwrYAB4fIlh-wqXa0hym1Mjewyrhy8Lo9JxonNZF7DqGFh1xt3-ouNt9S7Y9AWdwMHyNzAt5e2YLFpyEDzBE1w1p4dPvnwgbO2VfrvGRacww2FcSlSdI-rfD08HQsOf1-KzP2opu2Y7SyVFO6BklIDSSkQWkFBKoixzyOxwzF2W8oHk__igpzxXB4vWeVlbQxzMGS3rufubZWFJm_nty1iHWzfpkrJzsP8WAZnIUqRBAeGirNn6S8wb89CqRwID86BCFQNWsbenCO7jR38jTZ'
     INSTRUCTION = f.decrypt(INSTRUCTION_ENCRYPTED).decode()
     
-    html_data_response = pandas_ai.run(df, prompt=INSTRUCTION)
+    # html_data_response = pandas_ai.run(df, prompt=INSTRUCTION)
+
+    with st.spinner('Searching...'):
+        response = llm.responses.create(
+            instructions = INSTRUCTION,
+            prompt = f"Here is the dataframe: {df}",
+            model = model,
+            temperature = 0.6,
+        )
     
-    return html_data_response
+    return response
 
 # Definitive CSS selectors for Streamlit 1.45.1+
 st.markdown("""
@@ -83,7 +91,7 @@ if st.session_state.get('authentication_status'):
 
     # Model list, Vector store ID, assistant IDs (one for initial upload eval, 
     # the second for follow-up user questions).
-    # MODEL_LIST = ["gpt-4o-mini", "gpt-4.1-nano", "gpt-4.1-mini", "gpt-4.1"] #, "o4-mini"]
+    MODEL_LIST = ["gpt-4o-mini"] #, "gpt-4.1-nano", "gpt-4.1-mini", "gpt-4.1"] #, "o4-mini"]
     # VECTOR_STORE_ID = st.secrets["VECTOR_STORE_ID"]
     # MATH_ASSISTANT_ID = st.secrets["MATH_ASSISTANT_ID"]
     # MATH_ASSISTANT2_ID = st.secrets["MATH_ASSISTANT2_ID"]
@@ -93,7 +101,7 @@ if st.session_state.get('authentication_status'):
     st.header(":globe_with_meridians: Fastmap")
         
     # Retrieve user-selected openai model.
-    # model: str = st.selectbox("Model", options=MODEL_LIST)
+    model: str = st.selectbox("Model", options=MODEL_LIST)
     
     # Create advanced options dropdown with upload file option.
     with st.expander("Analysis Options", expanded=True):
