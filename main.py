172d969c2f670a618bca1c060b8d0a3c83180a7f
victor256sd <victor256@gmail.com>
2025-08-04 22:59:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,26 @@ def wait_on_run(client, run, thread):
 def get_response(client, thread):
     return client.beta.threads.messages.list(thread_id=thread.id, order="asc")
 
+def get_file_ids_from_thread(thread):
+    file_ids = [
+        file_id
+        for m in get_response(thread)
+        for file_id in m.file_ids
+    ]
+    return file_ids
+
+# Write Each File ID's Contents with Separator Implementation for Readability
+def write_file(file_id, count, output_path=output_path):
+    file_data = client.files.content(file_id) # Extract the content from the file ID
+    file_content = file_data.read() # Assign the content to a variable
+    separator_start = f'\n\n\n\nFILE # {count + 1}\n\n\n\n'
+    separator_end = '\n\n\n\n' + '#' * 100 + '\n\n\n\n'
+
+    with open(output_path, "ab") as file:
+        file.write(separator_start.encode())  # Encode the string to bytes
+        file.write(file_content) # Write the content
+        file.write(separator_end.encode())    # Encode the string to bytes
+
 # Disable the button called via on_click attribute.
 def disable_button():
     st.session_state.disabled = True        
@@ -114,6 +134,15 @@ def map_prep(df):
     # Wait on the run to complete, then retrieve messages from the thread.
     run = wait_on_run(llm, run, thread)
     messages = get_response(llm, thread)
+
+    # Iterate through the File IDs while Calling write_file for File Output
+    file_ids = get_file_ids_from_thread(thread_id) # Retrieve file IDs
+    print('\nFILE IDS: ', file_ids)
+    print('\nNUMBER OF FILE IDS: ', len(file_ids))
+    for count, file_id in enumerate(file_ids):
+        print(Fore.GREEN + f'\nWriting file #{count + 1}...\n')
+        write_file(file_id, count) # Write file ID contents
+        print(Fore.GREEN + f'File {count + 1} written.\n')
     
     i = 0
     html_chunks = []
